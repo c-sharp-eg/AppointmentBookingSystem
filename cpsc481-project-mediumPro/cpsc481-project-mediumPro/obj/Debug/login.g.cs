fb6d32edf6e4f21b80e9dc1bd3a34efe90318ada
danwong <danielwong33@hotmail.com>
2013-11-16 15:22:49 -0700

    added things
    - added some next/previous day button
    - moved the save button out of the edit panel
    - added view past appointments under patient bar on the top
    - made popup windows draggable

--- cpsc481-project-mediumPro/cpsc481-project-mediumPro/obj/Debug/login.g.cs
+++ cpsc481-project-mediumPro/cpsc481-project-mediumPro/obj/Debug/login.g.cs
@@ -9,7 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using Expression.Blend.SampleData.SampleDataSource;
 using System;
 using System.Diagnostics;
 using System.Windows;
