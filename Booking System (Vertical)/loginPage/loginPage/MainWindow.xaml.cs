d374b15d6473038a2f86416ab0f9bc6a8fa46c2e
danwong <danielwong33@hotmail.com>
2013-11-27 17:53:08 -0700

    window startup
    got it to open the main window from the login, also changed temp login
    info to username and password

---
+++ Booking System (Vertical)/loginPage/loginPage/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace loginPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //enter key check
+            this.KeyUp += new KeyEventHandler(loginButton_KeyUp);
+
+            //default pointer click position to the username login
+            usernameField.Focus();
+            usernameField.Select(0, 0);
+        }
+
+        // this is for letting the enter key to be used to login
+        void loginButton_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                checkLogin();
+            }
+        }
+
+        // exit button will close the program
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //login button will call the function to check fields
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            checkLogin();
+        }
+
+        // used for checking the user entered fields, also will return a little information status on the top (changes colour of te)
+        private void checkLogin()
+        {
+            //hardcoded username/password
+            //probably want this blank when we're testing
+            if ((usernameField.Text == "username") && (passwordField.Password == "password"))
+            {
+                statusText.Foreground = Brushes.Green;
+                statusText.Text = "Logging in...";
+
+                //opens the main booking window and closes the login screen 
+                calendar cal = new calendar();
+                cal.Show();
+                this.Close();
+            }
+
+            else
+            {
+                statusText.Foreground = Brushes.Red;
+                statusText.Text = "Incorrect username/password";
+                passwordField.Password = "";
+            }
+        }
+
+
+    }
+}
